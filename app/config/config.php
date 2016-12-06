947cddf9ea719f362a1ccad633850fc1821e7d6b
李铭昕 <715557344@qq.com>
2016-12-06 10:12:53 +0800

    * 发布版本

--- app/config/config.php
+++ app/config/config.php
@@ -22,7 +22,7 @@ return new \Phalcon\Config(
        | This value is version for this project.
        |
        */
-        'version' => '1.1.3',
+        'version' => '1.2.0',
 
         /*
         |--------------------------------------------------------------------------
